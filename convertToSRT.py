ea808546ddd3defd8bd6340bf93c103983f264c0
kauwers <kauwers@umich.edu>
2017-02-01 17:02:54 -0500

    minor comment updates

--- convertToSRT.py
+++ convertToSRT.py
@@ -1,7 +1,5 @@
 import re
 
-#Cycle through each file in the folder
-
 #for input - Gets each line in file and create a tuple with timestamp, content, then add them to a dictionary as strings {timestamp, text}
 def getTimedLines():
     file = open('test3.txt','r')
@@ -17,18 +15,14 @@ def getTimedLines():
             #catches other instances where timestamp and text are not on the same line
             elif re.findall(r'^[^\[][\w]+',line):
                 lineText = removeBadQuotes(str(re.findall(r'^[[\s|\S]+',line)))
-                print(lineText)
                 lineText = lineText[2:-2].strip()
                 addToTimedLine(lineTime, " part one " +lineText , timedLineDict)
             #catch instances where timestamp and text are on the same line
             elif re.findall(r'[0-9]+[\:][0-9]+[\:][0-9]+[\.][0-9]+', line):
                 lineTime = findTimeStamp(line)
                 lineTime = lineTime[2:-2].strip()
-
                 lineText = removeBadQuotes(str(re.findall(r'[\]][\s|\S]*', line)))
                 lineText = lineText[3:-2].strip()
-                print(lineText)
-
                 addToTimedLine(lineTime,  lineText, timedLineDict)
             else:
                 pass
@@ -47,10 +41,10 @@ def addToTimedLine(time, text, theDict):
         theDict[time] = theDict[time] + " " + text
     return theDict
 
+#for input
 def removeTabs(line):
     line=str(re.sub('\t|\r|\n', ' ', line)) #remove tabs from lines
     return line
-
 #fix for single quotes that occur between double quotes
 def removeBadQuotes(line):
     line=str(re.sub(r"\\","", line))
@@ -61,16 +55,15 @@ def findTimeStamp(line):
     time=str(re.findall(r'[0-9]+[\:][0-9]+[\:][0-9]+[\.][0-9]+', line))
     return time
 
+#for output - creates a new line every time there is a timestamp that starts with a capital letter
+
+#Cycle through each file in the folder
+
+
+
+#Function calls
 theDict = getTimedLines()
 for a in theDict:
     print(a+"      "+theDict[a])
-
-#for output - creates a new line every time there is a timestamp that starts with a capital letter
-
-#correct issue with single quotes formatting weird - maybe convert file format
-
-
-
-
 
 #rewrite idea - use timestamp as a trigger and start a new list item after, otherwise build a string  On export, split lines by searching for single words that begin with a capital letter followed by a colon to split out speaker lines.
